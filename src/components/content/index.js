aeafa9248b58f942aceba598a2036384b96df5f0
biekmurat <102846171+biekmurat@users.noreply.github.com>
2022-12-02 14:29:21 +0600

    Add files via upload

---
+++ src/components/content/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import  Logo  from "../../assets/icon-search.svg";
+import swCharacters from "../../data/characters.json";
+import swPlanets from "../../data/planets.json";
+import { CharacterLayout } from "../sw-character-layout";
+import { CharacterList } from "../sw-character-list";
+
+import { ReactComponent as PlanetSVG } from "../../assets/planet.svg";
+import { Select } from "../sw-select";
+
+export const Content = () => {
+  const [tab, setTab] = useState("Characters");
+
+  const handleChangeTab = (e) => {
+    setTab(e.target.value);
+  };
+
+  const planetRotationItem = (
+    <>
+      <PlanetSVG width={50} height={50} />
+      <p>planets</p>
+    </>
+  );
+
+  return (
+    <div className="content-layout">
+      <Select tab={tab} handleChangeTab={handleChangeTab} />
+
+      {tab === "Characters" &&
+        swCharacters.map((character) => (
+          <CharacterLayout key={character.name} item={character}>
+            <CharacterList label="Name" item={character.name} />
+              <CharacterList label="Club: " item={character.club}/>
+          </CharacterLayout>
+        ))}
+
+      {tab === "Zlodey" &&
+        swPlanets.map((planet) => (
+          <CharacterLayout key={planet.name} item={planet}>
+            <CharacterList label="name" item={planet.name} />
+              <CharacterList label="Club: " item={planet.club}/>
+          </CharacterLayout>
+        ))}
+    </div>
+  );
+};
---
+++ src/components/sw-character-layout/index.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import { DefaultContext } from "../../Contexts";
+
+export const CharacterLayout = ({ item, children }) => {
+  const { handleChooseFan } = useContext(DefaultContext);
+  return (
+    <div key={item.name} className="item-list-layout">
+      <div>{children}</div>
+      <div className="item-image-layout">
+        <img className="item-image" src={item.url} alt={item.name} />
+      </div>
+    </div>
+  );
+};
